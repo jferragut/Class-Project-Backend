a541c9d29dbcb238619e5a18adccb9759831e231
Gabriel Innecco <innecco9@gmail.com>
2018-01-26 18:37:46 +0000

    changes to manage py

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tictactoe.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "class-projectbackend.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -11,5 +11,6 @@ if __name__ == "__main__":
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) from exc
+        )
+        raise
     execute_from_command_line(sys.argv)
