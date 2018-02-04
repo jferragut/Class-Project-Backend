e50cb1941b6a1be9470c111389f3900feceb99a3
Jonathan <jonnywrites@gmail.com>
2018-02-04 18:57:51 +0000

    cleaned up URLs

--- api/urls.py
+++ api/urls.py
@@ -2,12 +2,24 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('user/<str:user_name>', views.UserView.as_view()),
+    #Request user information (GET)
+    path('user/<str:user_name>', views.UserView.as_view()), 
+    
+    #add coin to Watchlist(PUT) or Delete from Watchlist(DELETE)
     path('user/<str:user_name>/watchlist/<str:coin_symbol>', views.UserWatchlistView.as_view()),
-    path('user/<str:user_name>/watchlist', views.UserWatchlistDetailView.as_view()),
-    path('user/<str:user_name>/alert/<str:coin_symbol>', views.UpdateAlertsView.as_view()),
+    
+    #Request user's Watchlist (GET)
+    path('user/<str:user_name>/watchlist', views.UserWatchlistDetailView.as_view()), 
+    
+    #Add (PUT) or Delete currency(DELETE) from Alerts
+    path('user/<str:user_name>/alert/<str:coin_symbol>', views.UpdateAlertsView.as_view()), 
+    
+    #Request user Alerts (GET)
     path('user/<str:user_name>/alert', views.AlertsView.as_view()),
+    
+    #Request an individual currency's information(GET), Add (PUT), update (POST), or delete (DELETE) a currency from the database
     path('currency/<int:currency_id>', views.CurrencyView.as_view()),
+    
+    #Request a list of all currencies
     path('currencies/', views.CurrenciesView.as_view()),
-    path('currencies/<str:user_name>', views.CurrenciesView.as_view())
 ]
